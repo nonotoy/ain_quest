ce134ccae425edc35d671942eb79dfdfef2bd0d8
nonotoy <yosh.blum@gmail.com>
2023-05-20 19:03:18 +0900

    Applied changes to index.html, script.js, styles.css, and added SAMPLE.jpeg to materials folder

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ function showSection(sectionId) {
                 controls.classList.remove("hidden");
             }
 
-            // Default - No translation shown when open a page initially
+            // Set visibility of tips based on 'tipsVisible' variable
             for (let i = 0; i < tips.length; i++) {
-                tips[i].style.display = "none";
+                tips[i].style.display = tipsVisible ? "block" : "none";
             }
 
         } else {
@@ -42,12 +42,21 @@ $(document).ready(function () {
     showSection("start-page");
 });
 
-function updateDescription(lang) {
+function updateDescription(lang, tipLang = 'tips') {
     const descriptions = document.querySelectorAll('.description');
+    const tips = document.querySelectorAll('.tip');
+
     descriptions.forEach((description) => {
         const key = description.getAttribute('data-key');
         if (translations[lang] && translations[lang][key]) {
             description.textContent = translations[lang][key];
+        }
+    });
+
+    tips.forEach((tip) => {
+        const key = tip.getAttribute('data-key');
+        if (translations[tipLang] && translations[tipLang][key]) {
+            tip.textContent = translations[tipLang][key];
         }
     });
 }
@@ -61,19 +70,75 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    updateDescription('kana'); // Set default
+    updateDescription('kana', 'tips'); // Set default
+});
 
-    addGifHoverEffect();
+document.getElementById('kana').addEventListener('change', function() {
+    if(this.checked) {
+        updateDescription('kana', 'tips');
+    }
+});
+
+document.getElementById('roman').addEventListener('change', function() {
+    if(this.checked) {
+        updateDescription('roman', 'tips');
+    }
 });
 
 document.getElementById("tips").addEventListener("change", toggleTips);
 
 function toggleTips() {
     const tips = document.getElementsByClassName("tip");
-    const isChecked = document.getElementById("tips").checked;
+    tipsVisible = document.getElementById("tips").checked;
 
     for (let i = 0; i < tips.length; i++) {
-        tips[i].style.display = isChecked ? "block" : "none";
+        tips[i].style.display = tipsVisible ? "block" : "none";
     }
-
 }
+
+// Call the function immediately after defining it
+toggleTips();
+
+const translations = {
+    'kana': {
+        'title': '📚 シノッ　カンピソㇱ',
+
+        'qst_1': 'ネイタ　エオマン？',
+        'opt_1a': 'リㇰ',
+        'opt_1b': 'ノㇱキ',
+        'opt_1c': 'ラ',
+
+        'qst_1a': 'リㇰ　タ　ニㇱクㇽ　アン。',
+
+        'next': 'エトㇰタ　パイェアン。',
+        'returnToHome': 'エ＝シノッ　オケレ　ヤ？',
+        'appr': 'イヤイライケレ！',  
+        'home': 'チセ　アコホシピ。',
+    },
+    'roman': {
+        'title': '📚 Sinot Kampisos',
+
+        'qst_1': 'neyta e=oman?',
+        'opt_1a': 'rik',
+        'opt_1b': 'noski',
+        'opt_1c': 'ra',
+
+        'qst_1a': 'rik ta niskur an.',
+
+        'next': 'etokta paye=an.',
+        'returnToHome': 'e=sinot= okere ya?',
+        'appr': 'iyayraykere!',    
+        'home': 'cise a=kohosipi.',      
+    },
+    'tips': {
+        'qst_1-tip': 'どこに行きますか？',
+        'opt_1a-tip': '上',
+        'opt_1b-tip': '真ん中',
+        'opt_1c-tip': '下',  
+
+        'next-tip': '先に進む',
+        'returnToHome-tip': '終わりにしますか？',
+        'appr-tip': 'ありがとう！',
+        'home-tip': '最初に戻る',
+    }
+};
